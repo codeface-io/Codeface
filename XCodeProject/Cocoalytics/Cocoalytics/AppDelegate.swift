1bcd6564256f99c4da829f5e1ec90c16047f69a7
Sebastian <contact@flowtoolz.com>
2018-11-02 13:01:55 +0100

    create new xcode project

---
+++ XCodeProject/Cocoalytics/Cocoalytics/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Cocoalytics
+//
+//  Created by Sebastian ******** on 02.11.18.
+//  Copyright © 2018 Flowtoolz. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
