a9da820e7d8bd976aaa659c14b9d40713c513ab9
Sebastian <contact@flowtoolz.com>
2018-11-18 19:39:34 +0100

    move mapping loc -> color to dedicated file

--- Code/CodeFileAnalytics/CodeFileAnalytics.swift
+++ Code/CodeFileAnalytics/CodeFileAnalytics.swift
@@ -1,13 +1,3 @@
-import SwiftyToolz
-
-func warningColor(for linesOfCode: Int) -> Color.System
-{
-    if linesOfCode < 100 { return .green }
-    else if linesOfCode < 200 { return .yellow }
-    else if linesOfCode < 300 { return .orange }
-    else { return .red }
-}
-
 struct CodeFileAnalytics
 {
     init(file: CodeFile, loc: Int)
---
+++ Code/VisualizationModel/FileMetrics.swift
@@ -0,0 +1,9 @@
+import SwiftyToolz
+
+func warningColor(for linesOfCode: Int) -> Color.System
+{
+    if linesOfCode < 100 { return .green }
+    else if linesOfCode < 200 { return .yellow }
+    else if linesOfCode < 300 { return .orange }
+    else { return .red }
+}
