a5e206afbbf7b9a1332a13bcca006521def38f88
flowtoolz <contact@flowtoolz.com>
2020-10-29 00:48:03 +0100

    Add draft for SymbolCache

---
+++ Code/Project Tree/SymbolCache.swift
@@ -0,0 +1,35 @@
+import FoundationToolz
+import Foundation
+import SwiftObserver
+
+class SymbolCache
+{
+    init(inspector: ProjectInspector)
+    {
+        self.inspector = inspector
+    }
+    
+    func symbols(for codeFile: CodeFolder.CodeFile) -> SymbolPromise
+    {
+        Promise
+        {
+            promise in
+            
+            if let symbols = symbolsByFilePath[codeFile.path]
+            {
+                return promise.fulfill(.success(symbols))
+            }
+            
+            inspector.symbols(for: codeFile).whenFulfilled
+            {
+                do    { promise.fulfill(.success(try $0.get())) }
+                catch { promise.fulfill(.failure(error)) }
+            }
+        }
+    }
+    
+    typealias SymbolPromise = Promise<Result<[LSPDocumentSymbol], Error>>
+    
+    private let inspector: ProjectInspector
+    private var symbolsByFilePath = [String: [LSPDocumentSymbol]]()
+}
