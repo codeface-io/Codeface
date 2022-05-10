8a4224dfccf4f0b974ed5f39c9367d192c6accad
Sebastian Fichtner <contact@flowtoolz.com>
2022-05-10 20:16:50 +0100

    Visually distinguish document symbol artifacts

--- Code/main/ContentView.swift
+++ Code/main/ContentView.swift
@@ -1,6 +1,7 @@
 import SwiftUI
 import AppKit
 import SwiftObserver
+import SwiftLSP
 
 struct Preview: PreviewProvider
 {
@@ -40,11 +41,13 @@ struct ContentView: View
                 }
                 label:
                 {
-                    Image(systemName: systemName(for: artifact.kind))
-                        .foregroundColor(symbolColor(for: artifact.kind))
+                    Image(systemName: systemImageName(for: artifact.kind))
+                        .foregroundColor(iconColor(for: artifact.kind))
                     
                     Text(artifact.displayName)
                         .fixedSize()
+                        .font(.system(.title3, design: .for(artifact)))
+                    
                     Spacer()
                     
                     if let loc = artifact.metrics?.linesOfCode
@@ -69,26 +72,95 @@ struct ContentView: View
         }
     }
     
-    private func symbolColor(for artifactKind: CodeArtifact.Kind) -> Color
+    private func iconColor(for artifactKind: CodeArtifact.Kind) -> Color
     {
         switch artifactKind
         {
         case .folder: return Color(NSColor.secondaryLabelColor)
         case .file: return .white
-        case .symbol: return Color(NSColor.systemPurple)
+        case .symbol(let symbol): return iconColor(for: symbol)
         }
     }
     
-    private func systemName(for artifactKind: CodeArtifact.Kind) -> String
+    private func iconColor(for symbol: LSPDocumentSymbol) -> Color
+    {
+        guard let symbolKind = symbol.symbolKind else
+        {
+            return Color(NSColor.secondaryLabelColor)
+        }
+        
+        switch symbolKind
+        {
+        case .File, .Module, .Package:
+            return .white
+        case .Class, .Interface, .Struct, .Enum:
+            return Color(NSColor.systemPurple)
+        case .Namespace:
+            return Color(NSColor.systemOrange)
+        case .Method, .Constructor, .Function:
+            return Color(NSColor.systemBlue)
+        case .Property, .Field, .EnumMember:
+            return Color(NSColor.systemTeal)
+        case .Variable, .Constant:
+            return Color(NSColor.systemPink)
+        case .String:
+            return Color(NSColor.systemRed)
+        case .Number, .Boolean, .Array, .Object, .Key, .Null, .Event, .Operator, .TypeParameter:
+            return Color(NSColor.secondaryLabelColor)
+        }
+    }
+    
+    private func systemImageName(for artifactKind: CodeArtifact.Kind) -> String
     {
         switch artifactKind
         {
         case .folder: return "folder.fill"
         case .file: return "doc.fill"
-        case .symbol: return "c.square.fill"
+        case .symbol(let symbol): return iconSystemImageName(for: symbol)
+        }
+    }
+    
+    private func iconSystemImageName(for symbol: LSPDocumentSymbol) -> String
+    {
+        guard let symbolKind = symbol.symbolKind else
+        {
+            return "questionmark.square.fill"
+        }
+        
+        switch symbolKind
+        {
+        case .File:
+            return "doc.fill"
+        case .Module, .Package:
+            return "shippingbox.fill"
+        case .Class, .Interface, .Struct, .Enum:
+            return "t.square.fill"
+        case .Namespace:
+            return "x.square.fill"
+        case .Method, .Constructor, .Function:
+            return "f.square.fill"
+        case .Property, .Field, .EnumMember:
+            return "p.square.fill"
+        case .Variable:
+            return "v.square.fill"
+        case .Constant:
+            return "c.square.fill"
+        case .String:
+            return "s.square.fill"
+        case .Number, .Boolean, .Array, .Object, .Key, .Null, .Event, .Operator, .TypeParameter:
+            return "square.fill"
         }
     }
     
     @StateObject private var viewModel = ContentViewModel()
     @State var selectedArtifact: CodeArtifact?
+}
+
+extension Font.Design {
+    static func `for`(_ artifact: CodeArtifact) -> Font.Design {
+        switch artifact.kind {
+        case .symbol: return .monospaced
+        default: return .default
+        }
+    }
 }
