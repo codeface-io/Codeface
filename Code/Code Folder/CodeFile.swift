2003b34982559143002c54dc52db92fe91609c76
Sebastian Fichtner <contact@flowtoolz.com>
2022-05-30 21:24:46 +0100

    Remove useless code

--- Code/Code Folder/CodeFile.swift
+++ Code/Code Folder/CodeFile.swift
@@ -1,20 +1,5 @@
-extension CodeFile
-{
-    static var dummy: CodeFile
-    {
-        .init(name: "dummy file", path: "", lines: [])
-    }
-}
-
 struct CodeFile: Equatable
 {
-    init(name: String, path: String, lines: [String])
-    {
-        self.name = name
-        self.path = path
-        self.lines = lines
-    }
-    
     let name: String
     let path: String
     let lines: [String]
--- Code/Code Folder/CodeFolder.swift
+++ Code/Code Folder/CodeFolder.swift
@@ -2,15 +2,6 @@ import Foundation
 
 struct CodeFolder: Equatable
 {
-    init(url: URL,
-         files: [CodeFile],
-         subfolders: [CodeFolder])
-    {
-        self.url = url
-        self.files = files
-        self.subfolders = subfolders
-    }
-    
     let url: URL
     let files: [CodeFile]
     let subfolders: [CodeFolder]
