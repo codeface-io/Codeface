2ab86aa68195dd6dd510e442964862acf5db74b9
Sebastian Fichtner <contact@flowtoolz.com>
2022-07-28 16:25:44 +0200

    Add incoming dependencies to symbols (not tested yet)

--- Code/Code Artifact/Processing Pipeline/Add Dependencies/CodeSymbolArtifact+AddDependencies.swift
+++ Code/Code Artifact/Processing Pipeline/Add Dependencies/CodeSymbolArtifact+AddDependencies.swift
@@ -25,8 +25,62 @@ extension CodeSymbolArtifact
                 continue
             }
             
-            // TODO: identify the symbol with the outgoing dependency (source symbol) ...
-            referencingLocation.range // range (in the file) associated/overlapping with the symbol that depends on self (on this symbol)
+            guard let referencingSymbolArtifact = referencingFileArtifact.findSymbolArtifact(containing: referencingLocation.range) else
+            {
+                log(warning: "Could not find symbol artifact containing referencing range in file artifact for LSP document URI:\n\(referencingLocation.uri)")
+                continue
+            }
+            
+            incomingDependencies += referencingSymbolArtifact
         }
+    }
+}
+
+extension CodeFileArtifact
+{
+    func findSymbolArtifact(containing range: LSPRange) -> CodeSymbolArtifact?
+    {
+        for symbol in symbols
+        {
+            if let artifact = symbol.findSymbolArtifact(containing: range)
+            {
+                return artifact
+            }
+        }
+        
+        return nil
+    }
+}
+
+extension CodeSymbolArtifact
+{
+    func findSymbolArtifact(containing range: LSPRange) -> CodeSymbolArtifact?
+    {
+        // depth first!!! we want the deepest symbol that contains the range
+        for subsymbol in subSymbols
+        {
+            if let artifact = subsymbol.findSymbolArtifact(containing: range)
+            {
+                return artifact
+            }
+        }
+        
+        return self.range.contains(range) ? self : nil
+    }
+}
+
+extension LSPRange
+{
+    func contains(_ otherRange: LSPRange) -> Bool
+    {
+        if otherRange.start.line < start.line { return false }
+        if otherRange.start.line > end.line { return false }
+        
+        if otherRange.end.line < start.line { return false }
+        if otherRange.end.line > end.line { return false }
+        
+        // TODO: compare character positions as well if start or end lines are equal (4 equalitues to check ...)
+        
+        return true
     }
 }
