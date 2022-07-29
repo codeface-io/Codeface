507a0b2d194d528c1cac04f2afc9565a08d0ce38
Sebastian Fichtner <contact@flowtoolz.com>
2022-07-29 12:27:06 +0200

    Move LSPRange containment check to SwiftLSP

--- Code/Code Artifact/Processing Pipeline/Add Dependencies/CodeSymbolArtifact+AddDependencies.swift
+++ Code/Code Artifact/Processing Pipeline/Add Dependencies/CodeSymbolArtifact+AddDependencies.swift
@@ -107,30 +107,3 @@ extension CodeSymbolArtifact
         }
     }
 }
-
-extension LSPRange
-{
-    func contains(_ otherRange: LSPRange) -> Bool
-    {
-        if otherRange.start.line < start.line { return false }
-        if otherRange.start.line > end.line { return false }
-        
-        if otherRange.start.line == start.line,
-           otherRange.start.character < start.character { return false }
-        
-        if otherRange.start.line == end.line,
-           otherRange.start.character > end.character { return false }
-        
-        if otherRange.end.line < start.line { return false }
-        if otherRange.end.line > end.line { return false }
-        
-        if otherRange.end.line == start.line,
-           otherRange.end.character < start.character { return false }
-        
-        if otherRange.end.line == end.line,
-           otherRange.end.character > end.character { return false }
-        
-        return true
-    }
-}
-
