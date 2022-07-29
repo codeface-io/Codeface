e1e79d71491a9d3c7f8c88eead6e4cfcffc18471
Sebastian Fichtner <contact@flowtoolz.com>
2022-07-29 12:23:49 +0200

    Make detection of referencing symbols more precise

--- Code/Code Artifact/Processing Pipeline/Add Dependencies/CodeSymbolArtifact+AddDependencies.swift
+++ Code/Code Artifact/Processing Pipeline/Add Dependencies/CodeSymbolArtifact+AddDependencies.swift
@@ -115,10 +115,20 @@ extension LSPRange
         if otherRange.start.line < start.line { return false }
         if otherRange.start.line > end.line { return false }
         
+        if otherRange.start.line == start.line,
+           otherRange.start.character < start.character { return false }
+        
+        if otherRange.start.line == end.line,
+           otherRange.start.character > end.character { return false }
+        
         if otherRange.end.line < start.line { return false }
         if otherRange.end.line > end.line { return false }
         
-        // TODO: compare character positions as well if start or end lines are equal (4 equalitues to check ...)
+        if otherRange.end.line == start.line,
+           otherRange.end.character < start.character { return false }
+        
+        if otherRange.end.line == end.line,
+           otherRange.end.character > end.character { return false }
         
         return true
     }
