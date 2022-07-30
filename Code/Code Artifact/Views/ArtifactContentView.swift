2aa25da4cb22a17494163457d67236e5df25790c
Sebastian Fichtner <contact@flowtoolz.com>
2022-07-30 16:01:14 +0200

    Proof principle: connect different artifacts with lines

--- Code/Code Artifact/Views/ArtifactContentView.swift
+++ Code/Code Artifact/Views/ArtifactContentView.swift
@@ -10,8 +10,18 @@ struct ArtifactContentView: View
             
             ZStack
             {
-                ForEach(artifact.filteredParts)
+                if artifact.filteredParts.count > 1
                 {
+                    Line(from: .init(x: artifact.filteredParts[0].frameInScopeContent.centerX,
+                                     y: artifact.filteredParts[0].frameInScopeContent.centerY),
+                         to: .init(x: artifact.filteredParts[1].frameInScopeContent.centerX,
+                                   y: artifact.filteredParts[1].frameInScopeContent.centerY))
+                    .stroke(lineWidth: 10)
+                    .foregroundColor(.black)
+                }
+                
+                ForEach(artifact.filteredParts)
+                {   
                     ArtifactView(artifact: $0,
                                  viewModel: viewModel,
                                  ignoreSearchFilter: ignoreSearchFilter)
