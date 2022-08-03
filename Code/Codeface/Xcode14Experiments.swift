59a082954dfaf7ff880a6534ae3906aad4eb5d8f
Sebastian Fichtner <contact@flowtoolz.com>
2022-08-03 08:06:30 +0200

    Start taking notes about useful new stuff in macOS 13 and Xcode 14

---
+++ Code/Codeface/Xcode14Experiments.swift
@@ -0,0 +1,20 @@
+import SwiftUI
+
+/*
+@available(macOS 13.0, *)
+struct CodefaceView14: View
+{
+    var body: some View
+    {
+        // rebuild navigation structure ...
+        NavigationSplitView {
+            Text("Sidebar")
+        } content: {
+            Text("Content")
+        } detail: {
+            Text("Detail")
+        }
+    }
+}
+*/
+// also rebuild window stuff with window options in "View" menu ...
