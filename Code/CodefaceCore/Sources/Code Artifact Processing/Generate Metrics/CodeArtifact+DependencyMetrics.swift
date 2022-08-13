a96acd4758ad9fbb35612d57b560e1ed9366dd08
Sebastian Fichtner <contact@flowtoolz.com>
2022-08-13 09:36:33 +0200

    Fix: order components by their size instead of randomly

---
+++ Code/CodefaceCore/Sources/Code Artifact Processing/Generate Metrics/CodeArtifact+DependencyMetrics.swift
@@ -0,0 +1,48 @@
+@MainActor
+func writeDependencyMetrics<Part>(toParts scopeParts: [Part],
+                                  dependencies scopeDependencies: Edges<Part>)
+    where Part: CodeArtifact & Hashable & Identifiable
+{
+    // write component ranks by component size
+    let scopeGraph = Graph(nodes: Set(scopeParts), edges: scopeDependencies)
+    let components = scopeGraph.findComponents()
+    
+    var componentsWithSize: [(Set<Part>, Int)] = components.map
+    {
+        ($0, $0.reduce(0) { $0 + $1.linesOfCode })
+    }
+    
+    componentsWithSize.sort { $0.1 > $1.1 }
+    
+    for componentIndex in componentsWithSize.indices
+    {
+        let component = componentsWithSize[componentIndex].0
+        
+        for part in component
+        {
+            part.metrics.componentRank = componentIndex
+        }
+    }
+    
+    // write topological ranks within components
+    for componentNodes in components
+    {
+        let componentDependencies = scopeDependencies.reduced(to: componentNodes)
+        let componentGraph = Graph(nodes: componentNodes, edges: componentDependencies)
+        
+        let topologicalRanks = componentGraph.findTopologicalRanks()
+        
+        for (part, rank) in topologicalRanks
+        {
+            part.metrics.topologicalRankInComponent = rank
+        }
+    }
+    
+    // write numbers of dependencies
+    for part in scopeParts
+    {
+        part.metrics.ingoingDependenciesInScope = scopeDependencies.ingoing(to: part).count
+            
+        part.metrics.outgoingDependenciesInScope = scopeDependencies.outgoing(from: part).count
+    }
+}
--- Code/CodefaceCore/Sources/Code Artifact Processing/Generate Metrics/CodeSymbolArtifact+GenerateMetrics.swift
+++ Code/CodefaceCore/Sources/Code Artifact Processing/Generate Metrics/CodeSymbolArtifact+GenerateMetrics.swift
@@ -33,48 +33,6 @@ extension CodeSymbolArtifact
     }
 }
 
-@MainActor
-func writeDependencyMetrics<Part>(toParts scopeParts: [Part],
-                                  dependencies scopeDependencies: Edges<Part>)
-    where Part: CodeArtifact & Hashable & Identifiable
-{
-    // write (random) component ranks
-    let scopeGraph = Graph(nodes: Set(scopeParts), edges: scopeDependencies)
-    let components = Array(scopeGraph.findComponents())
-    
-    for componentIndex in components.indices
-    {
-        let component = components[componentIndex]
-        
-        for part in component
-        {
-            part.metrics.componentRank = componentIndex
-        }
-    }
-    
-    // write topological ranks within components
-    for componentNodes in components
-    {
-        let componentDependencies = scopeDependencies.reduced(to: componentNodes)
-        let componentGraph = Graph(nodes: componentNodes, edges: componentDependencies)
-        
-        let topologicalRanks = componentGraph.findTopologicalRanks()
-        
-        for (part, rank) in topologicalRanks
-        {
-            part.metrics.topologicalRankInComponent = rank
-        }
-    }
-    
-    // write numbers of dependencies
-    for part in scopeParts
-    {
-        part.metrics.ingoingDependenciesInScope = scopeDependencies.ingoing(to: part).count
-            
-        part.metrics.outgoingDependenciesInScope = scopeDependencies.outgoing(from: part).count
-    }
-}
-
 extension CodeSymbolArtifact: Hashable
 {
     public static func == (lhs: CodeSymbolArtifact,
