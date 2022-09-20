7e4f5e49df83d1ea843b9dd1a1a587453876c885
Sebastian Fichtner <contact@flowtoolz.com>
2022-09-20 20:54:36 +0200

    Ponder how to "jump back" to actor of self from result handler on arbitrary actor

--- Code/CodefaceCore/Sources/Codebase Processor/ProjectProcessorViewModel.swift
+++ Code/CodefaceCore/Sources/Codebase Processor/ProjectProcessorViewModel.swift
@@ -15,18 +15,13 @@ public class ProjectProcessorViewModel: ObservableObject
         {
             newState in
             
-            /// TODO: the compiler does not warn that we must – let alone enforce that we do – jump to our own actor (in this case the MainActor) or do it asynchronously when setting `self.processorState`. First of all: Why the fuck not? It's obviously mutating data across actors since the sink closure runs on actor `ProjectProcessor`. Second: How can we observe across actors more easily? We could use `.receive(on: DispatchQueue.main).sink` to receive the update on the main **queue**, but how can we receive it on any **actor**, in particular on actor `self`?
+            /// TODO: the compiler does not warn that we must – let alone enforce that we do – jump to our own actor (in this case the MainActor). First of all: Why the fuck not? The sink closure often runs on other actors than MainActor. Second: How can we observe across actors more easily? (If self was its own actor) We can use `.receive(on: DispatchQueue.main).sink` to receive the update on the main **queue**, but how can we receive it on any **actor**, in particular on actor `self`?
             
             Task
             {
                 await MainActor.run
                 {
-                    if self.codebaseName == nil, let codebaseName = newState.codebaseName
-                    {
-                        self.codebaseName = codebaseName
-                    }
-                    
-                    self.processorState = newState
+                    self.processorDidUpdate(toNewState: newState)
                 }
             }
         }
@@ -79,6 +74,16 @@ public class ProjectProcessorViewModel: ObservableObject
         didSet { pathBar.select(selectedArtifact) }
     }
     
+    private func processorDidUpdate(toNewState newState: ProjectProcessor.State)
+    {
+        if codebaseName == nil, let newCodebaseName = newState.codebaseName
+        {
+            codebaseName = newCodebaseName
+        }
+        
+        processorState = newState
+    }
+    
     @Published public private(set) var processorState: ProjectProcessor.State
     private var stateObservation: AnyCancellable?
     
@@ -113,3 +118,4 @@ private extension ProjectProcessor.State
         }
     }
 }
+
