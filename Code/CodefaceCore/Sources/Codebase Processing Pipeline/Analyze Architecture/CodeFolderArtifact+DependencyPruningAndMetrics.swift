2f1ebaa4043b2270fa29c09817165e8f9f6a43bf
Sebastian Fichtner <contact@flowtoolz.com>
2022-10-02 21:36:28 +0200

    Fix: request edges from minimum equivalent graph via node IDs since it's a copy

--- Code/CodefaceCore/Sources/Codebase Processing Pipeline/Analyze Architecture/CodeFolderArtifact+DependencyPruningAndMetrics.swift
+++ Code/CodefaceCore/Sources/Codebase Processing Pipeline/Analyze Architecture/CodeFolderArtifact+DependencyPruningAndMetrics.swift
@@ -45,44 +45,44 @@ private extension Graph where NodeValue: CodeArtifact & Identifiable
 {
     func pruneDependenciesAndCalculateDependencyMetrics()
     {
-        // write component ranks by component size
+        // write component ranks sorted by component size
         let components = findComponents()
-        
+
         var componentsWithSize: [(Set<Node>, Int)] = components.map
         {
             ($0, $0.sum { $0.value.linesOfCode })
         }
-        
+
         componentsWithSize.sort { $0.1 > $1.1 }
-        
+
         for componentIndex in componentsWithSize.indices
         {
             let component = componentsWithSize[componentIndex].0
-            
+
             for node in component
             {
                 node.value.metrics.componentRank = componentIndex
             }
         }
-        
+
         // analyze each component
         for component in components
         {
             let componentGraph = copy(includedNodes: OrderedSet(component))
             let componentCondensationGraph = componentGraph.makeCondensation()
-            
+
             // write scc numbers sorted by topology
             let condensationNodesSortedByAncestors = componentCondensationGraph
                 .findNumberOfNodeAncestors()
                 .sorted { $0.1 < $1.1 }
                 .map { $0.0 }
-            
+
             for condensationNodeIndex in condensationNodesSortedByAncestors.indices
             {
                 let condensationNode = condensationNodesSortedByAncestors[condensationNodeIndex]
-                
+
                 let condensationNodeContainsCycles = condensationNode.value.nodes.count > 1
-                
+
                 for sccNode in condensationNode.value.nodes
                 {
                     sccNode.value.metrics.sccIndexTopologicallySorted = condensationNodeIndex
@@ -92,30 +92,30 @@ private extension Graph where NodeValue: CodeArtifact & Identifiable
             
             // remove non-essential dependencies
             let minimumCondensationGraph = componentCondensationGraph.makeMinimumEquivalentGraph()
-            
+
             for componentDependency in componentGraph.edges
             {
-                // make sure this is a dependency between different condensation nodes and not with an SCC
+                // make sure this is a dependency between different condensation nodes and not within a SCC
                 let source = componentDependency.source
                 let target = componentDependency.target
-                
+
                 guard let sourceSCCIndex = source.value.metrics.sccIndexTopologicallySorted,
                       let targetSCCIndex = target.value.metrics.sccIndexTopologicallySorted
                 else
                 {
                     fatalError("At this point, artifacts shoud have their scc index set")
                 }
-                
+
                 let isDependencyWithinSCC = sourceSCCIndex == targetSCCIndex
-                
+
                 if isDependencyWithinSCC { continue }
-                
+
                 // find the corresponding edge in the condensation graph
                 let condensationSource = condensationNodesSortedByAncestors[sourceSCCIndex]
                 let condensationTarget = condensationNodesSortedByAncestors[targetSCCIndex]
-                let essentialEdge = minimumCondensationGraph.edge(from: condensationSource,
-                                                                  to: condensationTarget)
-                
+                let essentialEdge = minimumCondensationGraph.edge(from: condensationSource.id,
+                                                                  to: condensationTarget.id)
+
                 if essentialEdge == nil
                 {
                     remove(componentDependency)
