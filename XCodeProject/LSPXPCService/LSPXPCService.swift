7fc561c36776d9e5c7cd12818a75b69f18e8e6b0
Sebastian Fichtner <contact@flowtoolz.com>
2022-10-30 17:16:21 +0100

    Add XPC service target stub for replacing LSPService ASAP

---
+++ XCodeProject/LSPXPCService/LSPXPCService.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+/// This object implements the protocol which we have defined. It provides the actual behavior for the service. It is 'exported' by the service to make it available to the process hosting the service over an NSXPCConnection.
+class LSPXPCService: NSObject, LSPXPCServiceProtocol {
+    
+    /// This implements the example protocol. Replace the body of this class with the implementation of this service's protocol.
+    @objc func uppercase(string: String, with reply: @escaping (String) -> Void) {
+        let response = string.uppercased()
+        reply(response)
+    }
+}
---
+++ XCodeProject/LSPXPCService/LSPXPCServiceProtocol.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+/// The protocol that this service will vend as its API. This protocol will also need to be visible to the process hosting the service.
+@objc protocol LSPXPCServiceProtocol {
+    
+    /// Replace the API of this protocol with an API appropriate to the service you are vending.
+    func uppercase(string: String, with reply: @escaping (String) -> Void)
+}
+
+/*
+ To use the service from an application or other process, use NSXPCConnection to establish a connection to the service by doing something like this:
+
+     let connectionToService = NSXPCConnection(serviceName: "com.flowtoolz.LSPXPCService")
+     connectionToService.remoteObjectInterface = NSXPCInterface(with: LSPXPCServiceProtocol.self)
+     connectionToService.resume()
+
+ Once you have a connection to the service, you can use it like this:
+
+     if let proxy = connectionToService.remoteObjectProxy as? LSPXPCServiceProtocol {
+         proxy.uppercase(string: "hello") { aString in
+             NSLog("Result string was: \(aString)")
+         }
+     }
+
+ And, when you are finished with the service, clean up the connection like this:
+
+     connectionToService.invalidate()
+*/
