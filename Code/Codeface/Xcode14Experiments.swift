9473b64bdb8ba2a201d0cc6727dc9e7b79bcac9b
Sebastian Fichtner <contact@flowtoolz.com>
2022-12-10 22:41:32 +0100

    Try to observe TextField focus

--- Code/Codeface/Xcode14Experiments.swift
+++ Code/Codeface/Xcode14Experiments.swift
@@ -74,6 +74,7 @@ struct InspectorView: View {
             }
             .frame(maxWidth: .infinity, maxHeight: .infinity)
             .toolbar {
+                // TODO: fext field in toolbar does not recognize its focus ...
                 SearchField()
                 
                 Button {
@@ -99,13 +100,17 @@ struct SearchField: View {
             Image(systemName: "magnifyingglass")
                 .foregroundColor(.primary.opacity(0.55))
             
-            TextField("Search",
+            TextField("Search Field",
                       text: $searchTerm,
                       prompt: Text("Enter search term"))
+            .focused($isFocused)
             .textFieldStyle(.plain)
         }
         .padding(6)
-        .background(RoundedRectangle(cornerRadius: 6).fill(.clear)) // use this to color the bg
+        .background {
+            RoundedRectangle(cornerRadius: 6)
+                .fill(isFocused ? Color.primary.opacity(0.05) : .clear) // use this to color the bg
+        }
         .overlay {
             RoundedRectangle(cornerRadius: 6)
                 .stroke(.primary.opacity(0.08))
@@ -113,6 +118,7 @@ struct SearchField: View {
         .frame(minWidth: 200)
     }
     
+    @FocusState private var isFocused: Bool
     @State var searchTerm = ""
 }
 
