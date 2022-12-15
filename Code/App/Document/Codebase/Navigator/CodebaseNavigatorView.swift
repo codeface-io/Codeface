2e9601256be3a50400723557e0d8c301905d69ff
Sebastian Fichtner <contact@flowtoolz.com>
2022-12-15 11:06:26 +0100

    Make selection non-optional on UI level

--- Code/App/Document/Codebase/Navigator/CodebaseNavigatorView.swift
+++ Code/App/Document/Codebase/Navigator/CodebaseNavigatorView.swift
@@ -1,15 +1,23 @@
 import SwiftUI
 import CodefaceCore
+import SwiftyToolz
 
 struct CodebaseNavigatorView: View
 {
+    init(rootArtifact: ArtifactViewModel, codefaceDocument: CodefaceDocument) {
+        self.rootArtifact = rootArtifact
+        self.codefaceDocument = codefaceDocument
+        
+        _selectedArtifact = State(wrappedValue: rootArtifact)
+    }
+    
     var body: some View
     {
         NavigationStack
         {
             List([rootArtifact],
                  children: \.children,
-                 selection: $codefaceDocument.selectedArtifact)
+                 selection: $selectedArtifact)
             {
                 artifactVM in
 
@@ -19,15 +27,29 @@ struct CodebaseNavigatorView: View
                                  isSelected: artifactVM == codefaceDocument.selectedArtifact)
                 }
             }
-        }
-        .onAppear
-        {
-            Task { codefaceDocument.selectedArtifact = rootArtifact }
+        }.onChange(of: codefaceDocument.selectedArtifact) { newSelection in
+            guard newSelection != selectedArtifact else { return }
+            
+            if let newSelection {
+                log("CodefaceDocument.selectedArtifact was programmatically set to \(newSelection.codeArtifact.name)")
+                selectedArtifact = newSelection
+            } else {
+                log(warning: "CodefaceDocument.selectedArtifact was programmatically set nil. This should not happen.")
+            }
+        }.onAppear {
+            codefaceDocument.selectedArtifact = selectedArtifact
+        }.onChange(of: selectedArtifact) {
+            log("Navigator selection was interactively set to \($0.codeArtifact.name)")
+            codefaceDocument.selectedArtifact = $0
         }
     }
     
     let rootArtifact: ArtifactViewModel
+    
+    // TODO: directly bind our selection to a non-optional selection in the CodefaceDocument or some other view model. there should always be a selection in the context of the whole codebase analysis view since the data always has a root artifact!
     @ObservedObject var codefaceDocument: CodefaceDocument
+    
+    @State private var selectedArtifact: ArtifactViewModel
 }
 
 
