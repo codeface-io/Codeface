739c60f229b02b2958f8ccec0683dd484d949006
Sebastian Fichtner <contact@flowtoolz.com>
2022-12-16 11:37:58 +0100

    Adjust comment to last commit

--- Code/App/Document/Codebase/Navigator/CodebaseNavigatorView.swift
+++ Code/App/Document/Codebase/Navigator/CodebaseNavigatorView.swift
@@ -48,7 +48,7 @@ struct CodebaseNavigatorView: View
     // TODO: directly bind our selection to a non-optional selection in the CodefaceDocument or some other view model. there should always be a selection in the context of the whole codebase analysis view since the data always has a root artifact!
     var codefaceDocument: CodefaceDocument
     
-    // FIXME: as soon as we use anything other than the plain String ID as selection type, the list UI fucks up and rows cannot be selected anymore after a while ... we can't even wrap the id in a struct that only contains the id and is hashable by the id ... WTF apple ... this means we can only retrieve the actual selected artifact by hashing it via its ID, which then requires cache invalidation and weak references ... 🤮
+    // FIXME: as soon as we use anything other than the plain String ID as selection type, the list UI fucks up and rows cannot be selected anymore after a while ... we can't even wrap the id in a struct that only contains the id and is hashable by the id ... WTF apple ... this means every row has to observe the selected ID and set its view model as selected in the document when the ID matches ...
     @State private var selectedArtifactID: CodeArtifact.ID
 }
 
