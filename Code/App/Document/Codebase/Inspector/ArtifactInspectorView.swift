b37cf12215866211fe2c809f4d2c128557171df6
Sebastian Fichtner <contact@flowtoolz.com>
2022-12-20 14:47:50 +0100

    Improve inspector UI

--- Code/App/Document/Codebase/Inspector/ArtifactInspectorView.swift
+++ Code/App/Document/Codebase/Inspector/ArtifactInspectorView.swift
@@ -10,56 +10,67 @@ struct ArtifactInspectorView: View
         {
             Label
             {
-                Text(artifactVM.codeArtifact.name)
+                Text("Identity")
             }
             icon:
             {
-                Image(systemName: artifactVM.iconSystemImageName)
-                    .foregroundColor(.init(artifactVM.iconFillColor))
+                Image(systemName: "info.circle")
             }
             .font(.title3)
+            .foregroundColor(.secondary)
             
-            Text(artifactVM.codeArtifact.kindName)
-                .foregroundColor(.secondary)
-                .font(.title3)
-            
-            Divider()
-            
-            HStack
+            LabeledContent("Name")
             {
                 Label
                 {
-                    Text("Lines of code:")
+                    Text(artifactVM.codeArtifact.name)
                 }
                 icon:
                 {
-                    Image(systemName: "text.alignleft")
-                        .foregroundColor(.secondary)
+                    Image(systemName: artifactVM.iconSystemImageName)
+                        .foregroundColor(.init(artifactVM.iconFillColor))
                 }
-                
-                Spacer()
-                
+            }
+            
+            LabeledContent("Type")
+            {
+                Text(artifactVM.codeArtifact.kindName)
+            }
+
+            Divider()
+            
+            Label
+            {
+                Text("Size")
+            }
+            icon:
+            {
+                Image(systemName: "arrow.up.and.down.text.horizontal")
+            }
+            .font(.title3)
+            .foregroundColor(.secondary)
+            
+            LabeledContent("Lines of Code")
+            {
                 Text("\(artifactVM.codeArtifact.linesOfCode)")
                     .foregroundColor(.init(artifactVM.linesOfCodeColor))
             }
-            .font(.title3)
             
             Divider()
             
-            HStack
+            Label
             {
-                Label
-                {
-                    Text("Is itself in cycles:")
-                }
-                icon:
-                {
-                    Image(systemName: "exclamationmark.arrow.triangle.2.circlepath")
-                        .foregroundColor(.secondary)
-                }
-                
-                Spacer()
-                
+                Text("Cycles")
+            }
+            icon:
+            {
+                Image(systemName: "arrow.3.trianglepath")
+            }
+            .font(.title3)
+            .foregroundColor(.secondary)
+            
+            LabeledContent("Is Itself in Cycles")
+            {
                 let isInCycle = artifactVM.codeArtifact.metrics.isInACycle ?? false
                 
                 let cycleColor: SwiftyToolz.Color = isInCycle ? .rgb(1, 0, 0) : .rgb(0, 1, 0)
@@ -67,22 +78,9 @@ struct ArtifactInspectorView: View
                 Text("\(isInCycle ? "Yes" : "No")")
                     .foregroundColor(SwiftUI.Color(cycleColor))
             }
-            .font(.title3)
             
-            HStack
+            LabeledContent("Parts in Cycles")
             {
-                Label
-                {
-                    Text("Parts in cycles:")
-                }
-                icon:
-                {
-                    Image(systemName: "arrow.3.trianglepath")
-                        .foregroundColor(.secondary)
-                }
-                
-                Spacer()
-                
                 let cyclicPortion = artifactVM.codeArtifact.metrics.portionOfPartsInCycles
                 
                 let cycleColor = Color.rgb(0, 1, 0)
@@ -91,7 +89,6 @@ struct ArtifactInspectorView: View
                 Text("\(Int(cyclicPortion * 100))%")
                     .foregroundColor(SwiftUI.Color(cycleColor))
             }
-            .font(.title3)
         }
         .scrollContentBackground(.hidden)
         .background(Color(white: colorScheme == .dark ? 0.1568 : 0.9647))
