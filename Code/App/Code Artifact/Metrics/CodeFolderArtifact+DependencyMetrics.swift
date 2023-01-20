c7cd0b93867f93c4c387960eebfd307d6d940fd6
Sebastian Fichtner <contact@flowtoolz.com>
2023-01-20 18:46:46 +0100

    Bring back dependency metrics calculation (now separated from pruning)

---
+++ Code/App/Code Artifact/Metrics/CodeFolderArtifact+DependencyMetrics.swift
@@ -0,0 +1,102 @@
+import SwiftNodes
+import OrderedCollections
+import SwiftyToolz
+
+/** TODO:
+ * bring back graph sorting
+ * Adjust processor states (architecture creation states have merged)
+ * ... ?
+ */
+
+@BackgroundActor
+extension CodeFolderArtifact
+{
+    func calculateDependencyMetricsRecursively()
+    {
+        // depth first! this is important
+        for part in partGraph.values
+        {
+            switch part.kind
+            {
+            case .subfolder(let subfolder):
+                subfolder.calculateDependencyMetricsRecursively()
+            case .file(let file):
+                file.calculateDependencyMetricsRecursively()
+            }
+        }
+        
+        partGraph.calculateDependencyMetrics()
+    }
+}
+
+@BackgroundActor
+private extension CodeFileArtifact
+{
+    func calculateDependencyMetricsRecursively()
+    {
+        symbolGraph.values.forEach { $0.calculateDependencyMetricsRecursively() }
+        
+        symbolGraph.calculateDependencyMetrics()
+    }
+}
+
+@BackgroundActor
+private extension CodeSymbolArtifact
+{
+    func calculateDependencyMetricsRecursively()
+    {
+        subsymbolGraph.values.forEach { $0.calculateDependencyMetricsRecursively() }
+        
+        subsymbolGraph.calculateDependencyMetrics()
+    }
+}
+
+@BackgroundActor
+private extension Graph where NodeValue: CodeArtifact & Identifiable, NodeID == CodeArtifact.ID
+{
+    func calculateDependencyMetrics()
+    {
+        // write component ranks sorted by component size
+        let sortedComponents = findComponents()
+            .map { ($0, $0.sum { $0.value.linesOfCode }) }
+            .sorted { $0.1 > $1.1 }
+            .map { $0.0 }
+
+        sortedComponents.forEachIndex
+        {
+            component, componentIndex in
+            
+            for node in component
+            {
+                node.value.metrics.componentRank = componentIndex
+            }
+        }
+
+        // write cycle-related metrics (scc number, is part of cycle)
+        for component in sortedComponents
+        {
+            let componentIDs = Set(component.map({ $0.id }))
+            let componentGraph = subGraph(nodeIDs: componentIDs)
+            let componentCondensationGraph = componentGraph.makeCondensationGraph()
+
+            // write scc numbers sorted by topology
+            let sortedCondensationNodes = componentCondensationGraph
+                .findNumberOfNodeAncestors()
+                .sorted { $0.1 < $1.1 }
+                .map { $0.0 }
+
+            sortedCondensationNodes.forEachIndex
+            {
+                condensationNode, condensationNodeIndex in
+
+                let condensationNodeContainsCycles = condensationNode.value.nodes.count > 1
+
+                for node in condensationNode.value.nodes
+                {
+                    node.value.metrics.sccIndexTopologicallySorted = condensationNodeIndex
+                    node.value.metrics.isInACycle = condensationNodeContainsCycles
+                }
+            }
+        }
+    }
+}
--- Code/App/Codebase Processor/Codebase Processor/CodebaseProcessor.swift
+++ Code/App/Codebase Processor/Codebase Processor/CodebaseProcessor.swift
@@ -27,7 +27,7 @@ class CodebaseProcessor: ObservableObject
             await BackgroundActor.run
             {
                 codebaseArchitecture.calculateSizeMetricsRecursively()
-//                codebaseArchitecture.recursivelyPruneDependenciesAndCalculateDependencyMetrics()
+                codebaseArchitecture.calculateDependencyMetricsRecursively()
                 codebaseArchitecture.calculateCycleMetricsRecursively()
             }
             
