806c9e539524deeccd4b4c5e72f2f4e09dc675b2
Sebastian Fichtner <contact@flowtoolz.com>
2023-01-25 14:30:17 +0100

    Delete legacy code files that Xcode didn't delete when moving them to packages

--- Code/Framework-Candidates/SwiftUI+Helpers.swift
+++
@@ -1,9 +0,0 @@
-import SwiftUI
-
-func toggleSidebar()
-{
-    // https://stackoverflow.com/questions/61771591/toggle-sidebar-in-swiftui-navigationview-on-macos
-    NSApp.sendAction(#selector(NSSplitViewController.toggleSidebar(_:)),
-                     to: nil,
-                     from: nil)
-}
