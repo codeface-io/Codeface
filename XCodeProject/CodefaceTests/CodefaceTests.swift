ec9fabcb0ab0019599a070648c831a97fa03a4f9
Sebastian Fichtner <contact@flowtoolz.com>
2023-02-19 10:03:04 +0100

    Add a test that detects a bug

--- XCodeProject/CodefaceTests/CodefaceTests.swift
+++ XCodeProject/CodefaceTests/CodefaceTests.swift
@@ -1,34 +1,68 @@
-//
-//  CodefaceTests.swift
-//  CodefaceTests
-//
-//  Created by Sebastian ******** on 02.11.18.
-//  Copyright © 2018 Flowtoolz. All rights reserved.
-//
-
 import XCTest
 @testable import Codeface
+import SwiftLSP
+import SwiftyToolz
 
-class CodefaceTests: XCTestCase {
-
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+class CodefaceTests: XCTestCase
+{
+    @BackgroundActor func testHighlevelDependenciesAreDetectedInSpiteOfTrickyPathPrefix() throws
+    {
+        let range = LSPRange(start: .init(line: 0, character: 0),
+                              end: .init(line: 10, character: 0))
+        
+        let classALSPSymbol = LSPDocumentSymbol(name: "ClassA",
+                                                kind: 5,
+                                                range: range,
+                                                selectionRange: range)
+        
+        let referenceARange = LSPRange(start: .init(line: 1, character: 0),
+                                       end: .init(line: 2, character: 0))
+        
+        let referenceA = CodeSymbol.ReferenceLocation(filePathRelativeToRoot: "AB/AB.swift",
+                                                      range: referenceARange)
+        
+        let classA = try CodeSymbol(lspDocumentySymbol: classALSPSymbol,
+                                    referenceLocations: [referenceA],
+                                    children: [])
+        
+        let fileA = CodeFile(name: "A.swift",
+                             code: "",
+                             symbols: [classA])
+        
+        let folderA = CodeFolder(name: "A", files: [fileA])
+        
+        let classABLSPSymbol = LSPDocumentSymbol(name: "ClassAB",
+                                                 kind: 5,
+                                                 range: range,
+                                                 selectionRange: range)
+        
+        let classAB = try CodeSymbol(lspDocumentySymbol: classABLSPSymbol,
+                                     referenceLocations: [],
+                                     children: [])
+        
+        let fileAB = CodeFile(name: "AB.swift",
+                              code: "",
+                              symbols: [classAB])
+        
+        let folderAB = CodeFolder(name: "AB", files: [fileAB])
+        
+        let folder = CodeFolder(name: "Root", subfolders: [folderA, folderAB])
+        
+        var extraReferences = [CodeSymbol.ReferenceLocation]()
+        
+        let folderArtifact = CodeFolderArtifact(codeFolder: folder,
+                                                filePathRelativeToRoot: "",
+                                                additionalReferences: &extraReferences)
+        
+        let nodes = folderArtifact.partGraph.nodes
+        
+        guard let idAB = nodes.first(where: { $0.value.name == "AB" })?.id,
+              let idA = nodes.first(where: { $0.value.name == "A" })?.id
+        else
+        {
+            throw "Could not find node IDs for subfolder artifacts"
         }
+        
+        XCTAssert(folderArtifact.partGraph.containsEdge(from: idAB, to: idA))
     }
-
 }
